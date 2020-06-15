5bdd124b77fef1129d8481344067df14cef5a4c7
clovisnunes <clobnjuh@hotmail.com>
2020-06-15 03:06:21 -0300

    utils done

---
+++ controllers/utils.js
@@ -0,0 +1,19 @@
+function age (dateString) {
+    var birthday = +new Date(dateString);
+    return ~~((Date.now() - birthday) / (31557600000));
+}
+
+function graduation (num) {
+    switch(num) {
+        case "1":
+            return "Ensino Médio Completo";
+        case "2":
+            return "Ensino Superior Completo";
+        case "3":
+            return "Mestrado";
+        case "4":
+            return "Doutorado";
+    }
+}
+
+export default { age, graduation };
